8d022614fcdf65bf7da4d252df1fde09ec781dc3
iffishells <iffishells@gmail.com>
2020-08-08 13:14:42 +0500

    setting middleware and server the static files

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const app = express();
 const BodyParser = require('body-parser');
+const ejs = require('ejs');
+const app = express();
 
+//serving the static file
+app.use(express.static("public"))
+
+//for the middle ware
+app.use(BodyParser.urlencoded({extended : true}));
+
+//for the ejs engine
+app.set('view engine' , 'ejs')
 
 app.get('/', function (req, res) {
   res.send('Hello World')
