797428eece2de68db9855685ad398f0521063b86
iffishells <iffishells@gmail.com>
2020-08-10 22:07:30 +0500

    getting saving password at the local server

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const ejs = require('ejs');
 const path = require('path');
 const app = express();
 
+
+const member_list = []
+
 //serving the static file
 
 app.use(express.static(path.join(__dirname , 'public')));
@@ -18,15 +21,33 @@ app.set('view engine' , 'ejs')
 app.get('/', function (req, res) {
   res.render('index.ejs')
 });
+
+
 app.post("/" , function(req , resp)
 {
     console.log("root post method");
-    console.log(req.body.email);
+    console.log(req.body);
 });
+
 
 app.get('/contact', function (req, res) {
   res.render("contact.ejs")
 });
+
+
+app.post("/contact" , function(req, resp)
+{
+  const members = {
+    username  : req.body.username,
+    email: req.body.email ,
+    password : req.body.password
+  }
+  member_list.push(members)
+  resp.redirect("/")
+
+});
+
+
 app.listen(3000,function(req ,resp)
 {
   console.log("your server is listen at 3000 port")
