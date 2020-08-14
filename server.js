6456a219f2727a0c2aa84a2fe8c29376c56eb268
iffishells <iffishells@gmail.com>
2020-08-15 00:21:38 +0500

    addded front module and date Module

--- server.js
+++ server.js
@@ -5,75 +5,94 @@ const path = require('path');
 const app = express();
 
 
+// Varible defineds
+
+
+const adding_task_list = []
 const member_list = []
+
+console.log(member_list);
+
 //serving the static file
 
-app.use(express.static(path.join(__dirname , 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static("public"))
+
+
 //for the middle ware
-app.use(BodyParser.urlencoded({extended : true}));
+app.use(BodyParser.urlencoded({ extended: true }));
+
+
 
 //for the ejs engine
-app.set('views',path.join(__dirname , "views"))
-app.set('view engine' , 'ejs')
-
-const task_list = []
-// app.get('/', function (req, res) {
-//   res.render('index.ejs')
-// });
+app.set('views', path.join(__dirname, "views"))
+app.set('view engine', 'ejs')
 
 
-// app.post("/" , function(req , resp)
-// {
-//     const UserName = req.body.email
-//     const password = req.body.password
-//
-//     for (let i = 0; i < member_list.length; i++) {
-//       console.log("hellow ballah!");
-//       if (UserName == member_list[i].email && password == member_list[i].password  ) {
-//         resp.send("Connected Succcessfully")
-//       }
-//       else {
-//         resp.send("Your Password/Emails are Wrong! ")
-//       }
-//     }
-//     resp.render("todolist.ejs")
-//
-// });
 
-
-app.get('/contact', function (req, res) {
-  res.render("contact.ejs")
+app.get('/', function(req, res) {
+    res.render('index')
 });
 
 
-app.post("/contact" , function(req, resp)
-{
-  const members = {
-    username  : req.body.username,
-    email: req.body.email ,
-    password : req.body.password
-  }
-  member_list.push(members)
-  resp.redirect("/")
+app.post("/", function(req, resp) {
+    const UserName = req.body.email
+    const password = req.body.password
 
-});
+    for (let i = 0; i < member_list.length; i++) {
+        console.log("hellow ballah!");
+        if (UserName === member_list[i].email && password === member_list[i].password) {
+            console.log("in checking password ... ....");
+            resp.redirect("/todolist")
 
-app.get('/', function (req, res) {
-  res.render("todolist.ejs",{
-    newitem : task_list
-  })
-});
+        } else {
+            console.log("Your aren't registered")
+            resp.render("/")
+        }
+    }
 
-app.post('/', function (req, resp) {
 
-  task_list.push(req.body.set_items)
-  resp.redirect("/")
 });
 
 
 
-app.listen(3000,function(req ,resp)
-{
-  console.log("your server is listen at 3000 port")
+app.get('/contact', function(req, res) {
+    res.render("contact.ejs")
+});
+
+
+
+
+app.post("/contact", function(req, resp) {
+    const members = {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    }
+    member_list.push(members)
+    resp.redirect("/")
+
+});
+
+
+
+app.get('/todolist', function(req, res) {
+    console.log("todolist get has been called")
+    res.render("todolist", {
+        newitem: adding_task_list
+    });
+
+
+});
+
+app.post('/todolist', function(req, resp) {
+    console.log("todolist post has been called")
+    adding_task_list.push(req.body.set_items)
+    resp.redirect("/todolist")
+});
+
+
+
+app.listen(3000, function(req, resp) {
+    console.log("your server is listen at 3000 port")
 })
